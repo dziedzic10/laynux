08eab91a4f4e56cad85c7a740ec2ea7af3eecf2e
Andy <amdziedzic@proton.me>
2025-04-19 14:53:11 +0200

    stow other dirs

--- .install/install.sh
+++ .install/install.sh
@@ -9,15 +9,15 @@ makepkg -si
 cd ..
 rm -rf yay
 
-rm ~/.bashrc ~/.config/hypr/hyprland.conf
-yay -Rns dunst nano
+rm -f ~/.bashrc ~/.config/hypr/hyprland.conf
+yay -Rns --noconfirm dunst nano
 yay -Syu stow --noconfirm
 cd ~/laynux
 stow */
 sudo stow -t /etc .ly
+sudo stow -t /usr .grub_theme
 
 mapfile -t packages < dep
-
 yay -S --noconfirm --needed "${packages[@]}"
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim \
@@ -25,7 +25,12 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim \
 nvim --headless +"luafile ~/.config/nvim/lua/andy/packer.lua" +PackerSync +qa
 
 sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
-systemctl enable cronie tailscaled bluetooth waydroid-container
+crontab ~/laynux/.install/cron
+systemctl enable cronie tailscaled bluetooth 
+
+sudo sed -i '/^GRUB_THEME=/d' /etc/default/grub 
+echo 'GRUB_THEME="/usr/share/grub/themes/Xenlism-Arch/theme.txt"' | sudo tee -a /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "Installation completed, rebooting"
 sleep 1 && reboot
