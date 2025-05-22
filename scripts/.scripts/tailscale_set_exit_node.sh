159c344802139db398beb1098c5cd47b8a6f033d
Andy <amdziedzic@proton.me>
2025-05-22 10:40:33 +0200

    t

--- scripts/.scripts/tailscale_set_exit_node.sh
+++ scripts/.scripts/tailscale_set_exit_node.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-LOGFILE="$HOME/tailscale_set_exit_node.log"
 
 EXIT_NODE=$(tailscale exit-node suggest | sed -n 's/Suggested exit node: \(.*\)\./\1/p')
-echo "[$(date)] Suggested exit node: $EXIT_NODE"
+echo "Suggested exit node: $EXIT_NODE"
 
 sudo tailscale up --reset --exit-node="$EXIT_NODE"
-echo "[$(date)] Tailscale updated with new exit node."
+echo "Tailscale updated with new exit node."
 
 read -p "Do you want to allow LAN access? (Y/n) " lan_access
 
 if [[ "$lan_access" =~ ^[Nn]$ ]]; then
 	sudo tailscale set --exit-node-allow-lan-access=false
-	echo "[$(date)] LAN not allowed."
+	echo "LAN not allowed."
 else
 	sudo tailscale set --exit-node-allow-lan-access
-	echo "[$(date)] LAN allowed." 
+	echo "LAN allowed." 
 fi
 tailscale status
