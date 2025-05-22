8e7e9e878c39c5339336443aa5ba2b2bce84fe74
Andy <amdziedzic@proton.me>
2025-05-22 10:56:59 +0200

    t

--- scripts/.scripts/tailscale_set_exit_node.sh
+++ scripts/.scripts/tailscale_set_exit_node.sh
@@ -6,13 +6,23 @@ echo "Suggested exit node: $EXIT_NODE"
 sudo tailscale up --reset --exit-node="$EXIT_NODE"
 echo "Tailscale updated with new exit node."
 
-read -p "Do you want to allow LAN access? (Y/n) " lan_access
+read -p "Do you want to allow LAN access? (N/y) " lan_access
 
-if [[ "$lan_access" =~ ^[Nn]$ ]]; then
-	sudo tailscale set --exit-node-allow-lan-access=false
-	echo "LAN not allowed."
-else
+if [[ "$lan_access" =~ ^[Yy]$ ]]; then
 	sudo tailscale set --exit-node-allow-lan-access
 	echo "LAN allowed." 
+else
+	sudo tailscale set --exit-node-allow-lan-access=false
+	echo "LAN not allowed."
+fi
+
+read -p "Do you want to accept routes? (N/y) " accept_routes
+
+if [[ "$accept_routes" =~ ^[Yy]$ ]]; then
+	sudo tailscale set --accept-routes
+	echo "Routes allowed." 
+else
+	sudo tailscale set --accept-routes=false
+	echo "Routes not allowed."
 fi
 tailscale status
